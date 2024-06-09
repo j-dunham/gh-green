1e97861ef21b084d4d438c32fb6c7e7d22728402
j-dunham <18723510+j-dunham@users.noreply.github.com>
2024-06-08 23:30:24 -0400

    Add basic logic to get green status for the day

--- main.go
+++ main.go
@@ -2,25 +2,47 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/cli/go-gh/v2/pkg/api"
+	"log"
+	"time"
 )
 
-func main() {
-	fmt.Println("hi world, this is the gh-green extension!")
-	client, err := api.DefaultRESTClient()
-	if err != nil {
-		fmt.Println(err)
-		return
+// Need for wrapping the from argument so the type matches the expected input type
+type DateTime struct{ time.Time }
+
+func ExampleContributionGraphQL() {
+	opts := api.ClientOptions{
+		EnableCache: true,
+		Timeout:     5 * time.Second,
 	}
-	response := struct {Login string}{}
-	err = client.Get("user", &response)
+	client, err := api.NewGraphQLClient(opts)
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
-	fmt.Printf("running as %s\n", response.Login)
+
+	var Query struct {
+		Viewer struct {
+			ContributionsCollection struct {
+				TotalCommitContributions int
+				HasAnyContributions      bool
+			} `graphql:"contributionsCollection(from: $from)"`
+		} `graphql:"viewer"`
+	}
+
+	err = client.Query("contributionQuery", &Query, map[string]interface{}{"from": DateTime{time.Now()}})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	isGreen := Query.Viewer.ContributionsCollection.HasAnyContributions
+	if isGreen {
+		// print sparkle cake emoji
+		fmt.Println("🍰 You are green!")
+	} else {
+		fmt.Println("You haven't made any commits today... yet!")
+	}
 }
 
-// For more examples of using go-gh, see:
-// https://github.com/cli/go-gh/blob/trunk/example_gh_test.go
+func main() {
+	ExampleContributionGraphQL()
+}
